9912af048d718292d2d49d65c819c225a84ddbe7
novvac <novvac21@protonmail.com>
2021-02-10 21:29:46 +0100

    created drawers prototypes

--- client/src/components/core/LeftDrawer.vue
+++ client/src/components/core/LeftDrawer.vue
@@ -4,13 +4,35 @@
         floating
         clipped
     >
-        this is left nav drawer
+        <v-list class="ma-0 pa-0">
+            <v-list-item
+                v-for="link in links"
+                :key="link.icon"
+                :to="link.to"
+            >
+                <v-icon>mdi-{{link.icon}}</v-icon>
+
+                {{link.text}}
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
 </template>
 
 <script>
 export default {
     name: "CoreLeftDrawer",
+    data() {
+        return {
+            links: [
+                {icon: "home", text: "Home", to: "/app"},
+                {icon: "account-multiple", text: "Friends", to: "/app/friends"},
+                {icon: "calendar-blank", text: "Events", to: "/app/events"},
+                {icon: "video", text: "Watch videos", to: "/app/videos"},
+                {icon: "folder-image", text: "Photos", to: "/app/photos"},
+                {icon: "file-multiple", text: "Files", to: "/app/files"},
+            ]
+        }
+    }
 }
 </script>
 
--- client/src/components/core/RightDrawer.vue
+++ client/src/components/core/RightDrawer.vue
@@ -4,13 +4,54 @@
         clipped
         right
     >
-        this is right nav drawer
+        <v-list
+            v-for="item in items"
+            :key="item.subheader"
+            class="pa-0"
+        >
+            <v-subheader>{{item.subheader}}</v-subheader>
+            <v-list-item
+                v-for="subitem in item.content"
+                :key="subitem.to"
+                :to="subitem.to"
+            >
+                <v-avatar size="36">
+                    <v-img :src="subitem.avatar"></v-img>
+                </v-avatar>
+
+                {{subitem.text}}
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
 </template>
 
 <script>
 export default {
     name: "CoreRightDrawer",
+    data() {
+        return {
+            items: [
+                {
+                    subheader: "Your pages",
+                    content: [
+                        {avatar: "https://i.pinimg.com/736x/d6/a9/57/d6a957f1d8045c9c973c12bf5968326f.jpg", text: "Cyntia Kaleskov", to: "/app/pages/cyntia-kaleskov"},
+                    ]
+                },
+                {
+                    subheader: "Friends",
+                    content: [
+                        {avatar: "https://i.pinimg.com/736x/d6/a9/57/d6a957f1d8045c9c973c12bf5968326f.jpg", text: "Cyntia Kaleskov", to: "/app/pages/cyntia-kaleskov"},
+                    ]
+                },
+                {
+                    subheader: "Groups",
+                    content: [
+                        {avatar: "https://i.pinimg.com/736x/d6/a9/57/d6a957f1d8045c9c973c12bf5968326f.jpg", text: "Cyntia Kaleskov", to: "/app/pages/cyntia-kaleskov"},
+                    ]
+                }
+            ],
+        }
+    }
 }
 </script>
 
