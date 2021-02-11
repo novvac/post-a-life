e1383ab7c84ddf7207106b9f3b72cc0e846c5586
novvac <novvac21@protonmail.com>
2021-02-11 22:36:52 +0100

    added mongoose

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(cors());
+
+// db
+mongoose.connect("mognodb://localhost:27017/post-a-life", {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connection.once("open", () => {
+    console.log("[DATABASE] database is running");
+})
+mongoose.connection.on("error", () => {
+    console.log("[DATABASE] [ERR] database is not connected!");
+})
 
 app.listen(PORT, () => {
     console.log("Server is running!");
