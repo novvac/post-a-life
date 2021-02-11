186c5ece511894720476bcc46a466d63c23b2578
novvac <novvac21@protonmail.com>
2021-02-11 22:03:23 +0100

    added some liblaries to server

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log("Server is running!");
