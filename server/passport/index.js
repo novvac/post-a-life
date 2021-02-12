9ee0aafd4e5ef59ec03a031b3463ab59726ca22c
novvac <novvac21@protonmail.com>
2021-02-12 16:26:26 +0100

    added serializer and deserializer user

--- server/passport/index.js
+++ server/passport/index.js
@@ -3,6 +3,16 @@ const LocalStrategy = require("passport-local").Strategy;
 const User = require('../models/User.model');
 const bcrypt = require("bcrypt");
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+})
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    })
+})
+
 passport.use(new LocalStrategy({usernameField: "email"}, function(email, password, done) {
     User.findOne({email: email}, (err, user) => {
         if(err)
