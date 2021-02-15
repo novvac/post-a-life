f7324381dcb396f656dd7161e1a81e036d12942a
novvac <novvac21@protonmail.com>
2021-02-15 15:18:26 +0100

    added friends mechanism - not complete

--- server/routes/user.routing.js
+++ server/routes/user.routing.js
@@ -22,4 +22,48 @@ router.get("/basic-data/:id", passport.authenticate("jwt", {session: false}), (r
     })
 })
 
+router.get("/friend-status/:id", passport.authenticate("jwt", {session: false}), (req, res) => {
+    User.findOne({short_id: req.params.id}, (err, user) => {
+        if(err)
+            return res.status(500).send("Błąd serwera!");
+
+        if(user) {
+            //sprawdź czy user nie sprawdza znajomości ze samym sobą
+
+            let userA = req.user.friends.includes(user._id);
+            let userB = user.friends.includes(req.user._id);
+
+            res.status(200);
+
+            if(!userA && !userB)
+                return res.json({icon: "account-plus", text: "Dodaj do znajomych", status: "N"});
+            else if(userA && userB)
+                return res.json({icon: "account-multiple", text: "Jesteście znajomymi", status: "F"});
+            else if(userA && !userB)
+                return res.json({icon: "check", text: "Wysłano zaproszenie", status: "P"});
+            else if(!userA && userB)
+                return res.json({icon: "account-alert", text: "Otrzymano zaproszenie", status: "R"});
+        } else {
+            return res.status(404).send("Użytkownik nie istnieje!");
+        }
+    })
+})
+
+router.post("/add-friend/", passport.authenticate("jwt", {session: false}), (req, res) => {
+    const id = req.body.id;
+    if(!req.user.friends.includes(id)) {
+        User.findOne({id: req.user._id}, (err, user) => {
+            console.log(user);
+        })
+    }
+})
+
+router.post("/harden-relation/", passport.authenticate("jwt", {session: false}), (req, res) => {
+    console.log(req.body);
+})
+
+router.post("/destroy-relation/", passport.authenticate("jwt", {session: false}), (req, res) => {
+    console.log(req.body);
+})
+
 module.exports = router;
