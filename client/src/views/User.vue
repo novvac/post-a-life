a081b2e5eaa028ef90e8ef7579173ddf090abb00
novvac <novvac21@protonmail.com>
2021-02-15 15:18:47 +0100

    added friends mechanism - not complete

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -21,14 +21,39 @@
 
             <v-divider class="mt-5"></v-divider>
             <v-row class="ma-0 mx-5 my-3" align="center">
-                <div v-if="id != user.short_id">
-                    <v-btn text class="caption text-capitalize elevation-0">
-                    <v-icon class="mr-3" small>mdi-account-plus</v-icon>
-                        Dodaj do znajomych
-                    </v-btn>
+                <div v-if="id != user.short_id" class="d-flex">
+                    <!-- FRIEND BUTTON MECHANISM -->
+                    <base-menu>
+                        <template v-slot:activator>
+                            <v-btn 
+                                text 
+                                class="caption text-capitalize elevation-0"
+                                :color="basicData.friendStatus.status != 'N' ? 'primary' : undefined"
+                                @click="basicData.friendStatus.options.length === 0 ? invitationManager('/add-friend/') : undefined"
+                            >
+                                <v-icon class="mr-2" small>mdi-{{basicData.friendStatus.icon}}</v-icon>
+                                {{basicData.friendStatus.text}}
+                            </v-btn>
+                        </template>
+
+                        <base-card without-padding v-if="basicData.friendStatus.options">
+                            <v-list class="pa-0" dense>
+                                <v-list-item
+                                    v-for="option in basicData.friendStatus.options"
+                                    :key="option.icon"
+                                    link
+                                    @click="invitationManager(option.endpoint)"
+                                >
+                                    <v-icon small class="mr-2">mdi-{{option.icon}}</v-icon>
+                                    <span class="caption">{{option.text}}</span>
+                                </v-list-item>
+                            </v-list>
+                        </base-card>
+                    </base-menu>
+                    <!-- END FRIEND BUTTON MECHANISM -->
 
                     <v-btn text class="caption text-capitalize elevation-0 ml-2">
-                        <v-icon class="mr-3" small>mdi-message</v-icon>
+                        <v-icon class="mr-2" small>mdi-message</v-icon>
                         Wyślij wiadomość
                     </v-btn>
                 </div>
@@ -84,13 +109,40 @@ export default {
         loadBasicData() {
             return this.$http.get("http://192.168.43.5:3000/api/user/basic-data/" + this.id);
         },
+        loadFriendStatus() {
+            return this.$http.get("http://192.168.43.5:3000/api/user/friend-status/" + this.id);
+        },
         LOAD_USER() {
             this.msg = null;
             this.basicData = {},
             this.loaded = false;
-            this.$http.all([this.loadBasicData()])
-                .then(this.$http.spread((basicData) => {
+            this.$http.all([
+                this.loadBasicData(),
+                this.loadFriendStatus(),
+            ])
+                .then(this.$http.spread((basicData, friendStatus) => {
                     this.basicData = basicData.data;
+                    this.basicData.friendStatus = friendStatus.data;
+
+                    const status = friendStatus.data.status;
+                    if(status === "F") {
+                        this.setOptions([
+                            {icon: "account-minus", text: "Usuń ze znajomych", endpoint: '/destroy-relation/'}
+                        ])
+                    } else if(status === "P") {
+                        this.setOptions([
+                            {icon: "undo", text: "Cofnij wysyłanie zaproszenia", endpoint: '/destroy-relation/'},
+                        ])
+                    } else if(status === "R") {
+                        this.setOptions([
+                            {icon: "check", text: "Zaakceptuj", endpoint: '/harden-relation/'},
+                            {icon: "delete", text: "Usuń zaproszenie", endpoint: '/destroy-relation/'},
+                        ])
+                    } else {
+                        // this must be empty
+                        this.setOptions([]);
+                    }
+
                     this.loaded = true;
                 }))
                 .catch(err => {
@@ -102,6 +154,19 @@ export default {
                         
                     this.msg = err.response.data;
                     this.loaded = true;
+                })
+        },
+        setOptions(opts) {
+            this.basicData.friendStatus.options = opts;
+        },
+        invitationManager(endpoint) {
+            let url = "http://192.168.43.5:3000/api/user" + endpoint;
+            this.$http.post(url, {id: this.id})
+                .then(res => {
+                    console.log(res);
+                })
+                .catch(err => {
+                    console.log(err);
                 })
         }
     },
