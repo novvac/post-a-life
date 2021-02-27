78ca6e9fe78660362408b43d91a99732a932ef4f
novvac <novvac21@protonmail.com>
2021-02-28 00:51:39 +0100

    initial events layout - hero section

---
+++ client/src/components/Events/hero.vue
@@ -0,0 +1,25 @@
+<template>
+    <div
+        class="event-hero pa-5 white--text black"
+        :style="{width: breakpoint ? '50%' : '100%'}"
+    >
+        {{breakpoint}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "EventHero",
+    computed: {
+        breakpoint() {
+            return this.$vuetify.breakpoint.mdAndUp;
+        },
+    }
+}
+</script>
+
+<style lang="scss">
+.event-hero {
+    border-radius: 16px;
+}
+</style>
---
+++ client/src/views/Events.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="events ma-5">
+        <div class="hero">
+            <p class="ma-0 caption">
+                Najpopularniejsze wśród znajomych
+            </p>
+            <span class="font-weight-bold display-1">
+                Wydarzenia
+            </span>
+
+            <div class="hero-box mt-5">
+                <event-hero style="right: 0;"/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Events",
+    components: {
+        EventHero: () => import('@/components/Events/hero'),
+    }
+}
+</script>
+
+<style lang="scss">
+.hero-box {
+
+}
+</style>
